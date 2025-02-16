fdd6100c5a31518c13984ee2b6d33af5d69b82b4
Fahmi Zain <fahmizain000@gmail.com>
2025-02-16 22:54:42 +0700

    add font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:nutriscan_fe_flutter/routes/app_router.dart';
 import 'package:nutriscan_fe_flutter/utils/app_colors.dart';
 
@@ -18,6 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
         scaffoldBackgroundColor: AppColors.white,
+        textTheme: GoogleFonts.signikaTextTheme(),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: AppColors.primary,
