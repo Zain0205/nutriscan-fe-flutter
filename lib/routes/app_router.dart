d69efd0f0f318f6ec5491621a26a083e3d935835
Fahmi Zain <fahmizain000@gmail.com>
2025-02-18 16:00:56 +0700

    Update app_router.dart

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:nutriscan_fe_flutter/screens/auth/login/login_screen.dart';
 import 'package:nutriscan_fe_flutter/screens/auth/register/register_screen.dart';
 import 'package:nutriscan_fe_flutter/screens/onboarding/onboarding_screen.dart';
 import 'package:nutriscan_fe_flutter/screens/splash/splash_page.dart';
@@ -7,7 +8,11 @@ final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: "/", builder: (context, state) => const SplashPage()),
-    GoRoute(path: "/onboarding", builder: (context, state) => const OnboardingScreen()),
-    GoRoute(path: "/register", builder: (context, state) => const RegisterScreen()),
+    GoRoute(
+        path: "/onboarding",
+        builder: (context, state) => const OnboardingScreen()),
+    GoRoute(
+        path: "/register", builder: (context, state) => const RegisterScreen()),
+    GoRoute(path: "/login", builder: (context, state) => const LoginScreen()),
   ],
 );
