c1b06232a01d3d92b6ee3ae107733dcd605f7f9f
Fahmi Zain <fahmizain000@gmail.com>
2025-03-10 22:26:57 +0700

    Create auth_event.dart

---
+++ lib/blocs/auth/auth_event.dart
@@ -0,0 +1,77 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class RegisterStepOne extends AuthEvent {
+  final String? name;
+  final String? email;
+  final String? password;
+
+  RegisterStepOne({this.name, this.email, this.password});
+
+  @override 
+  List<Object?> get props => [name, email, password];
+}
+
+class RegisterStepTwo extends AuthEvent {
+  final int? age;
+  final double? weight;
+  final double? height;
+  final String? goal;
+
+  RegisterStepTwo({this.age, this.weight, this.height, this.goal});
+
+  @override
+  List<Object?> get props => [age, weight, height, goal];
+}
+
+class UpdateRegisterStepOneField extends AuthEvent {
+  final String? name;
+  final String? email;
+  final String? password;
+
+  UpdateRegisterStepOneField({
+    this.name,
+    this.email,
+    this.password,
+  });
+
+  @override
+  List<Object?> get props => [name, email, password];
+}
+
+class UpdateRegisterStepTwoField extends AuthEvent {
+  final int? age;
+  final double? weight;
+  final double? height;
+  final String? goal;
+
+  UpdateRegisterStepTwoField({
+    this.age,
+    this.weight,
+    this.height,
+    this.goal,
+  });
+
+  @override
+  List<Object?> get props => [age, weight, height, goal];
+}
+
+class GoBackStepOneEvent extends AuthEvent {}
+
+class RegisterEvent extends AuthEvent {}
+
+class LoginEvent extends AuthEvent {
+  final String? email;
+  final String? password;
+
+  LoginEvent({this.email, this.password});
+
+  @override
+  List<Object?> get props => [email, password];
+}
+
+class LogoutEvent extends AuthEvent {}
