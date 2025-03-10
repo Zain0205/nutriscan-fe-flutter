7c7b53f0e1d1027776a428ca22c54f8a0c233e88
Fahmi Zain <fahmizain000@gmail.com>
2025-03-10 22:27:27 +0700

    Update registration_button.dart

--- lib/widget/registration_button.dart
+++ lib/widget/registration_button.dart
@@ -1,65 +1,20 @@
 import "package:flutter/material.dart";
 
 class RegistrationButton extends StatelessWidget {
-  const RegistrationButton({required this.isSingleButton, super.key});
-  final bool isSingleButton;
+  const RegistrationButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        isSingleButton
-            ? ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(
-                    double.infinity,
-                    50,
-                  ),
-                ),
-                child: const Text('Next'),
-              )
-            : Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(
-                          double.infinity,
-                          50,
-                        ),
-                      ),
-                      child: const Text('Previous'),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(
-                          double.infinity,
-                          50,
-                        ),
-                      ),
-                      child: const Text('Next'),
-                    ),
-                  ),
-                ],
-              ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Already have an account?"),
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                "Login",
-                style: TextStyle(),
-              ),
-            ),
-          ],
+        const Text("Already have an account?"),
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            "Login",
+            style: TextStyle(),
+          ),
         ),
       ],
     );
