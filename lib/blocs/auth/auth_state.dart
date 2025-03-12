37b9477ede5e2bf07b93b1e1c902d86dc8fd6c5c
Fahmi Zain <fahmizain000@gmail.com>
2025-03-12 22:52:57 +0700

    Update auth_state.dart

--- lib/blocs/auth/auth_state.dart
+++ lib/blocs/auth/auth_state.dart
@@ -77,6 +77,26 @@ class AuthStepTwoState extends AuthState {
   List<Object?> get props => [name, email, password, age, weight, height, goal];
 }
 
+class AuthOtpVerification extends AuthState {
+  final String? email;
+  final String? otp;
+
+  AuthOtpVerification({this.email, this.otp});
+
+  AuthOtpVerification copyWith({
+    String? email,
+    String? otp,
+  }) {
+    return AuthOtpVerification(
+      email: email ?? this.email,
+      otp: otp ?? this.otp,
+    );
+  }
+
+  @override
+  List<Object?> get props => [email, otp];
+}
+
 class AuthFailure extends AuthState {
   final String message;
 
